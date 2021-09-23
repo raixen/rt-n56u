35b79269882fcaceff630c44453202fa872485c7
raixen <xys1014@163.com>
2021-09-23 18:12:38 +0800

    modify frp script

--- trunk/user/frp/frp_script.sh
+++ trunk/user/frp/frp_script.sh
@@ -3,10 +3,9 @@
 killall frpc frps
 mkdir -p /tmp/frp
 #启动frp功能后会运行以下脚本
-#frp项目地址教程: https://github.com/fatedier/frp/blob/master/README_zh.md
-#请自行修改 token 用于对客户端连接进行身份验证
 # IP查询： http://119.29.29.29/d?dn=github.com
 
+#请自行修改下列配置信息，token用于对客户端连接进行身份验证
 cat > "/tmp/frp/myfrpc.ini" <<-\EOF
 # ==========客户端配置：==========
 [common]
@@ -19,14 +18,16 @@ token = 12345
 #log_max_days = 3
 
 [web]
-remote_port = 6000
 type = http
 local_ip = 192.168.2.1
 local_port = 80
 subdomain = test
+use_encryption = true
+use_compression = true
 #host_header_rewrite = 实际你内网访问的域名，可以供公网的域名不一致，如果一致可以不写
-# ====================
+# =========客户端配置结束=========
 EOF
+
 
 #请手动配置【外部网络 (WAN) - 端口转发 (UPnP)】开启 WAN 外网端口
 cat > "/tmp/frp/myfrps.ini" <<-\EOF
@@ -44,10 +45,12 @@ max_pool_count = 50
 #log_file = /tmp/frps.log
 #log_level = info
 #log_max_days = 3
-# ====================
+# =========服务端配置结束=========
 EOF
 
-#启动：
+
+
+#启动脚本：
 frpc_enable=`nvram get frpc_enable`
 frps_enable=`nvram get frps_enable`
 
@@ -55,7 +58,8 @@ if [ "$frpc_enable" = "1" ] ; then
 	frpc_bin="/usr/bin/frpc"
 	if [ ! -f "$frpc_bin" ]; then
 		if [ ! -f "/tmp/frp/frpc" ];then
-			wget -c -P /tmp/frp https://github.com/etion2008/aaron/raw/main/frpc/frpc
+			wget -c -P /tmp/frp https://raw.fastgit.org/etion2008/aaron/raw/main/frpc/frpc
+			#wget -c -P /tmp/frp https://github.com/etion2008/aaron/raw/main/frpc/frpc
 			if [ ! -f "/tmp/frp/frpc" ]; then
 				logger -t "FRPC" "frpc二进制文件下载失败，可能是地址失效或者网络异常！"
 				nvram set frpc_enable=0
@@ -77,7 +81,8 @@ if [ "$frps_enable" = "1" ] ; then
 	frps_bin="/usr/bin/frps"
 	if [ ! -f "$frps_bin" ]; then
 		if [ ! -f "/tmp/frp/frps" ];then
-			wget -c -P /tmp/frp https://github.com/etion2008/aaron/raw/main/frps/frps
+			wget -c -P /tmp/frp https://raw.fastgit.org/etion2008/aaron/raw/main/frps/frps
+			#wget -c -P /tmp/frp https://github.com/etion2008/aaron/raw/main/frps/frps
 			if [ ! -f "/tmp/frp/frps" ]; then
 				logger -t "FRPS" "frps二进制文件下载失败，可能是地址失效或者网络异常！"
 				nvram set frps_enable=0
