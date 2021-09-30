fe424034f8d4ed5b12e812d3ff362ab56b95eed0
raixen <xys1014@163.com>
2021-10-01 01:02:41 +0800

    Update

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -593,20 +593,16 @@ function show_menu(L1, L2, L3){
 		menuL2_title[4] = "";
 		menuL2_link[5] = "";  //remove Firewall
 		menuL2_title[5] = "";
-		menuL2_link[9] = "";
-		menuL2_title[9] = "";
-		menuL2_link[10] = "";  //remove
-		menuL2_title[10] = "";
-		menuL2_link[11] = "";
-		menuL2_title[11] = "";
-		menuL2_link[12] = "";
-		menuL2_title[12] = "";
-		menuL2_link[13] = "";
-		menuL2_title[13] = "";
-		menuL2_link[15] = "";
-		menuL2_title[15] = "";
-		menuL2_link[16] = "";
-		menuL2_title[16] = "";
+		tablink[10] = "";
+		tabtitle[10] = "";  //remove
+		tablink[11] = "";
+		tabtitle[11] = "";
+		tablink[12] = "";
+		tabtitle[12] = "";
+		tablink[13] = "";
+		tabtitle[13] = "";
+		tablink[16] = "";
+		tabtitle[16] = "";
 
 		
 		if (lan_proto == '1'){
